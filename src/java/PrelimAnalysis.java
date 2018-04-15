7d23550a2a557b046311a90170e9712408298f5c
Luv Mehta <mehluv@gmail.com>
2018-04-15 12:09:30 +0530

    Added mapping of dialogue to characters

--- src/java/PrelimAnalysis.java
+++ src/java/PrelimAnalysis.java
@@ -1,28 +1,54 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PrelimAnalysis {
+    static final String TOM_CRUISE = "Tom Cruise";
+    static final String MAGLU = "Maglu";
+    static final String PUGSY = "Pugsy";
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new FileReader("SampleChapter.txt"));
-        //TODO: Replace with map of person to list of dialogues spoken by them
-        List<String> listOfDialogue = new ArrayList<>();
+        List<String> characters = new ArrayList<>();
+        characters.add(TOM_CRUISE);
+        characters.add(MAGLU);
+        characters.add(PUGSY);
+        Map<String, List<String>> mapOfDialogue = new HashMap<>();
+        characters.forEach(character -> mapOfDialogue.put(character, new ArrayList<>()));
         //System.out.println(br.lines().filter(el -> el.equals("")).count());
         String line = br.readLine();
-        while(line != null) {
-            if(!line.equals("")) {
+        Deque<String> lastTwoReferencedCharacters = new ArrayDeque<>();
+        while (line != null) {
+            if (!line.equals("")) {
+                String referencedCharacter = null;
+                //TODO: Make sure characters are recognized even if parts of their names are used
+                //TODO: Accomodate nicknames
+                for (String character : characters) {
+                    if (line.contains(character)) {
+                        referencedCharacter = character;
+                        if(lastTwoReferencedCharacters.size() == 2) {
+                            lastTwoReferencedCharacters.pop();
+                        }
+                        lastTwoReferencedCharacters.push(referencedCharacter);
+                        break;
+                    }
+                }
                 Matcher m = Pattern.compile("\"[A-Za-z,.?!'\\- ]+\"").matcher(line);
                 while (m.find()) {
-                    for (int i = 0; i <= m.groupCount(); i++) {
-                        listOfDialogue.add(m.group(i));
+                    if(referencedCharacter == null) {
+                        referencedCharacter = lastTwoReferencedCharacters.pop();
+                        lastTwoReferencedCharacters.push(referencedCharacter);
                     }
+                    mapOfDialogue.get(referencedCharacter).add(m.group());
                 }
             }
             line = br.readLine();
         }
-        System.out.println(listOfDialogue);
+        mapOfDialogue.entrySet().forEach(entry -> {
+            System.out.println("For character "+ entry.getKey());
+            entry.getValue().forEach(dialogue -> System.out.println("dialogue = " + dialogue));
+        });
     }
 }
